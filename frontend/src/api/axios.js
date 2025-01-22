bad6ab76bf71eaa92927a427db1d7ae1fdf19866
sarabenyahya91 <dev.test.rsmi@gmail.com>
2025-01-22 20:28:44 +0100

    deploy: Mise à jour de l URL du backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000', // Remplacez par l'URL de votre backend
+    baseURL: 'https://crm-supmti.onrender.com', // Remplacez par l'URL de votre backend
     headers: {
         'Content-Type': 'application/json',
     },
